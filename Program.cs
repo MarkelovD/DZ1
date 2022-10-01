e77fb347c37bd1aefc926ce468a42bb18662c8be
Dmitry <markelov16@gmail.com>
2022-10-01 21:45:25 +0500

    задача 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// ######## ЗАДАЧА №1 ######################
+Console.WriteLine("введи первое число:");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введи второе число:");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1>num2){
+     Console.WriteLine($"max={num1}");
+}
+ else if (num2>num1){
+     Console.WriteLine($"max={num2}");;
+}
