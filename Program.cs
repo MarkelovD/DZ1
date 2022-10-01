7d70ab07e09575e2621a007b9c6fb5524fcf397b
Dmitry <markelov16@gmail.com>
2022-10-01 21:46:09 +0500

    задача 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
 ﻿// ######## ЗАДАЧА №1 ######################
+// Console.WriteLine("введи первое число:");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("введи второе число:");
+// int num2 = int.Parse(Console.ReadLine()!);
+// if (num1>num2){
+//      Console.WriteLine($"max={num1}");
+// }
+//  else if (num2>num1){
+//      Console.WriteLine($"max={num2}");;
+// }
+//######## ЗАДАЧА №2 ######################
+
 Console.WriteLine("введи первое число:");
 int num1 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("введи второе число:");
 int num2 = int.Parse(Console.ReadLine()!);
-if (num1>num2){
-     Console.WriteLine($"max={num1}");
+Console.WriteLine("введи третье число:");
+int num3 = int.Parse(Console.ReadLine()!);
+if (num1>num2 & num1>num3){
+    Console.WriteLine($"max={num1}");;
 }
- else if (num2>num1){
-     Console.WriteLine($"max={num2}");;
+else if (num2>num1 & num2>num3){
+    Console.WriteLine($"max={num2}");
 }
+else if (num3>num1 & num3>num2){
+    Console.WriteLine($"max={num3}");
+ }
